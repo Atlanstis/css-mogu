44ceb367061ed795b22dfffef2eff498d40ef14e
Gypsophila <futur_@163.com>
2022-05-13 10:58:23 +0800

    Theme 层

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,15 @@
   <router-view />
 </template>
 
+<script>
+import { onMounted } from 'vue'
+export default {
+  setup() {
+    onMounted(() => {
+      window.document.documentElement.setAttribute('data-theme', 'default')
+    })
+  },
+}
+</script>
+
 <style lang="scss"></style>
